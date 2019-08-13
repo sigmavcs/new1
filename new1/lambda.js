a44c7b7e56017a7cbfbb855202fe665f62ab029a
sigmavcs <jchamath+sigma@gmail.com>
2019-08-13 15:34:09 +0530

    ss

--- new1/lambda.js
+++ new1/lambda.js
@@ -2,4 +2,3 @@ let AWS = require('aws-sdk');
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = function (event, context, callback) {
-    callback(null, { "message": "Successfully executed!" });
