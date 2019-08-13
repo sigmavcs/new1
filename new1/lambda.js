7ab8899f4e358b6b31eb5df8b0f27bbb48057321
sigmavcs <jchamath+sigma@gmail.com>
2019-08-13 10:44:56 +0530

    04

--- new1/lambda.js
+++ new1/lambda.js
@@ -2,17 +2,5 @@ let AWS = require('aws-sdk');
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = function (event, context, callback) {
-    cognito_idp.listUsers({
-        UserPoolId: "us-east-1_D10y3fy0o",
-        AttributesToGet: ["given_name", "picture"],
-        Limit: 10
-    }, function (error, data) {
-        if (error) {
-            // implement error handling logic here
-            throw error;
-        }
-        // your logic goes within this block
-    });
-
     callback(null, { "message": "Successfully executed!!!" });
 }
